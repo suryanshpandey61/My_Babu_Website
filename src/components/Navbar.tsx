bfbb0214a4d294949ee32f9ccb56d0d05d82b4dd
suryanshpandey61 <suryansh.knipss061@gmail.com>
2025-03-23 12:38:06 +0530

    Poppins Font added

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav>
+        <div>
+            <span className='font-bold'>My</span><span>babu</span>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
+import Navbar from '@/components/Navbar'
 import React from 'react'
+import '../app/globals.css'
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({
+    weight:['300','400','500','600'],
+    subsets:['latin']
+})
 
 function index() {
   return (
-    <div>index</div>
+    <div className={poppins.className}>
+        <Navbar/>
+    </div>
   )
 }
 
