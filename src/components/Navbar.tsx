4640d80f1c41bd3d4ddf8c23fa8c9275300066c4
suryanshpandey61 <suryansh.knipss061@gmail.com>
2025-03-23 13:04:16 +0530

    Navbar Added Completly

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <nav>
+    <nav className='w-[1040px]  p-[16px] rounded-[30px] h-[66px] items-center shadow-2xl  flex justify-between top-[27px] fixed left-[80px]'>
         <div>
             <span className='font-bold'>My</span><span>babu</span>
+        </div>
+        <div className='gap-x-[16px] flex'>
+            <a href="">Services</a>
+            <a href="">Work</a>
+            <a href="">FAQs</a>
+            <a href="">Contact Us</a>
+        </div>
+        <div>
+            <button className='bg-[#45BF6F] text-[14px] text-white px-2 py-2 hover:cursor-pointer rounded-md'>Get Started</button>
         </div>
     </nav>
   )
