20b078e1927bb08f3c43c3b1d8f3624475b3dbf3
suryanshpandey61 <suryansh.knipss061@gmail.com>
2025-03-24 13:24:22 +0530

    Grid added

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 function Work() {
   return (
-    <div>Work</div>
+    <div className='w-[85%] mx-auto mt-[3%]'>
+        {/* heading div  */}
+        <div>
+            <h1 className='text-[36px]'>How Mybabu works?</h1>
+        </div>
+        {/* grid-4 div  */}
+        <div className='grid-cols-4 grid mt-[2%]'>
+            <div>Submit your Request</div>
+            <div>Expert Matching</div>
+            <div>Recieve Assistant</div>
+            <div>Track Progress</div>
+        </div>
+
+
+    </div>
   )
 }
 
