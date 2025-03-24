af7cea01333c4066c4e6742a5f2e7d9785378af5
suryanshpandey61 <suryansh.knipss061@gmail.com>
2025-03-24 11:56:08 +0530

    Navbar updated

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import Image from 'next/image'
+import heroSectionUser from '../assests/heroSectionUser.png'
 function HeroSection() {
   return (
-    <div className=''>
-        <div className='w-[762px] gap-[20px] text-center top-[136px] fixed left-[219px]'>
+    <div className='w-full'>
+        <div className='w-[90%] gap-[20px] text-center '>
               <h1 className='text-[36px] text-[#004016]'>
                 Your Personal Assistant for Hassle-Free 
                 Government Paper
@@ -12,6 +13,9 @@ function HeroSection() {
               <div className='mt-[4%]'>
                   <button className='bg-[#45BF6F] text-[14px] text-white px-2 py-2 hover:cursor-pointer rounded-md'>Get Started</button>
                   <button className='font-bold px-4'>How It Works</button>
+              </div>
+              <div>
+                <Image src={heroSectionUser} alt="User Image"/>
               </div>
         </div>
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <nav className='w-[1040px]  p-[16px] rounded-[30px] h-[66px] items-center shadow-2xl  flex justify-between top-[27px] fixed left-[80px]'>
+   <div className='w-full'>
+     <nav className=' w-[90%] z-50 mt-[2%]  p-[16px] rounded-[30px] h-[66px] items-center shadow-2xl  flex justify-between mx-auto'>
         <div>
             <span className='font-bold'>My</span><span>babu</span>
         </div>
@@ -16,6 +17,7 @@ function Navbar() {
             <button className='bg-[#45BF6F] text-[14px] text-white px-2 py-2 hover:cursor-pointer rounded-md'>Get Started</button>
         </div>
     </nav>
+   </div>
   )
 }
 
