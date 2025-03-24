2ef67532919ad1f8d9f4f82a437ec148a5efb436
suryanshpandey61 <suryansh.knipss061@gmail.com>
2025-03-24 12:56:19 +0530

    Hero Section Updated

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
 import heroSectionUser from '../assests/heroSectionUser.png'
-import rentAgreementImg from '../assests/RentAgreementImg.png'
+import rentAgreementImg from '../assests/rentAgreementImg.png'
 import jobsAvailablebtn from '../assests/jobsAvailablebtn.png'
+import dottedLineImg from '../assests/dottedLineImg.png'
 function HeroSection() {
   return (
     <div className='w-full'>
-        <div className='w-[70%] mt-[5%] mx-auto  gap-[20px] text-center '>
+        <div className='w-[70%] border mt-[5%] mx-auto  gap-[20px] text-center '>
               <h1 className='text-[36px] font-semibold text-[#004016]'>
                 Your Personal Assistant for Hassle-Free 
                 Government Paperwork
@@ -16,26 +17,34 @@ function HeroSection() {
                   <button className='bg-[#45BF6F] text-[14px] text-white px-2 py-2 hover:cursor-pointer rounded-md'>Get Started</button>
                   <button className='font-bold px-4'>How It Works</button>
               </div>
-              <div className='mx-auto border mt-[6%] text-center flex justify-center'>
+              <div className='mx-auto  mt-[6%] text-center flex justify-center'>
                 <Image
                 src={jobsAvailablebtn}
                 alt='Jobs Available btn'
                 style={{width:'140px',height:'44px'}}
-                className='mt-[5%]'
+                className='mt-[8%] left-[18%] relative'
+                />
+                <Image
+                src={dottedLineImg}
+                alt='dooted-line-image'
+                className='absolute mt-[2.5%]'
                 />
                 <Image 
                 src={heroSectionUser} 
                 alt="User Image" 
-                style={{height:'350px',width:'350px'}}
-                className=''
+                style={{height:'410px',width:'410px'}}
+                className='relative  -right-[4%]'
                 />
                 <Image
                 src={rentAgreementImg}
                 alt='Rent Aggrement Image'
-                style={{height:'255px',width:'265px'}}
-                className='mt-[3%] right-[12%] relative'
+                style={{height:'300px',width:'300px'}}
+                className='mt-[3%] right-[9.5%] relative'
                 />
               </div>
+        </div>
+        <div>
+          
         </div>
     </div>
   )
