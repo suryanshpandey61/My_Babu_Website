d4d13b9dcd92c45596013477f29c0dd218b3acbe
suryanshpandey61 <suryansh.knipss061@gmail.com>
2025-03-26 16:46:37 +0530

    Footer completed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import ReactanlgeImg from '../assests/Rectangle 27.png'
 import Image from 'next/image'
 function Footer() {
   return (
-    <div className='w-full'>
+    <div className='w-full mb-[2%]'>
  
             <div className='w-[85%] mx-auto mt-[6%] flex justify-between'>
                     {/* left img div  */}
@@ -13,29 +13,29 @@ function Footer() {
                         alt='rectangle-image'
                         />
                     </div>
-                    <div className='flex w-[50%] justify-evenly'> 
+                    <div className='flex w-[50%]  justify-between'> 
                             {/* 1 col  */}
                             <div className='flex flex-col'> 
-                                <h1 className='leading-[28px]'>Quick Links</h1>
-                                <a href="">Home</a>
-                                <a href="">About Us</a>
-                                <a href="">Services</a>
-                                <a href="">How it Works</a>
-                                <a href="">Contact Us</a>
+                                <h1 className='leading-[28px] font-bold text-[16px]'>Quick Links</h1>
+                                <a href="" className='text-[16px] leading-[28px] font-normal'>Home</a>
+                                <a href="" className='text-[16px] leading-[28px] font-normal'>About Us</a>
+                                <a href="" className='text-[16px] leading-[28px] font-normal'>Services</a>
+                                <a href="" className='text-[16px] leading-[28px] font-normal'>How it Works</a>
+                                <a href="" className='text-[16px] leading-[28px] font-normal'>Contact Us</a>
                             </div>
                             {/* 2 col  */}
                             <div className='flex flex-col'>
-                                    <h1>Socials</h1>
-                                    <a href="">X</a>
-                                    <a href="">Linked In</a>
-                                    <a href="">Facebook</a>
-                                    <a href="">Instagram</a>
+                                    <h1 className='leading-[28px] font-bold text-[16px]'>Socials</h1>
+                                    <a href="" className='text-[16px] leading-[28px] font-normal'>X</a>
+                                    <a href="" className='text-[16px] leading-[28px] font-normal'>Linked In</a>
+                                    <a href="" className='text-[16px] leading-[28px] font-normal'>Facebook</a>
+                                    <a href="" className='text-[16px] leading-[28px] font-normal'>Instagram</a>
                             </div>
                             {/* 3 col  */}
                             <div className='flex flex-col'>
-                                <h1>Legal</h1>
-                                <h2>Privacy Policy</h2>
-                                <h2>Terms & Conditions</h2>
+                                <h1 className='leading-[28px] font-bold text-[16px]'>Legal</h1>
+                                <h2 className='text-[16px] leading-[28px] font-normal'>Privacy Policy</h2>
+                                <h2 className='text-[16px] leading-[28px] font-normal'>Terms & Conditions</h2>
                             </div>
                     </div>
             </div>
