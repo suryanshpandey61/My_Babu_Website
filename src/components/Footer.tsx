903573dc73bf8ddb9ff625de3196de0a45df2935
suryanshpandey61 <suryansh.knipss061@gmail.com>
2025-03-26 16:28:13 +0530

    Footed updation

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className='w-full'>
  
-            <div className='w-[85%]'>
+            <div className='w-[85%] flex justify-between'>
                     {/* left img div  */}
                     <div>
                         <Image
@@ -15,13 +15,27 @@ function Footer() {
                     </div>
                     <div> 
                             {/* 1 col  */}
-                            <div>
+                            <div className='flex flex-col'> 
                                 <h1>Quick Links</h1>
                                 <a href="">Home</a>
                                 <a href="">About Us</a>
                                 <a href="">Services</a>
                                 <a href="">How it Works</a>
                                 <a href="">Contact Us</a>
+                            </div>
+                            {/* 2 col  */}
+                            <div>
+                                    <h1>Socials</h1>
+                                    <a href="">X</a>
+                                    <a href="">Linked In</a>
+                                    <a href="">Facebook</a>
+                                    <a href="">Instagram</a>
+                            </div>
+                            {/* 3 col  */}
+                            <div>
+                                <h1>Legal</h1>
+                                <h2>Privacy Policy</h2>
+                                <h2>Terms & Conditions</h2>
                             </div>
                     </div>
             </div>
