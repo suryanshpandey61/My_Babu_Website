1e6551a6a545d1e1b81b7a2227f85dbe9e9bb057
Suryansh Pandey <144314814+suryanshpandey61@users.noreply.github.com>
2025-04-04 10:50:29 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ import Footer from "@/components/Footer";
 import ContactUs from "@/components/ContactUs";
 
 const poppins = Poppins({
-  weight: ["300", "400", "500", "600"],
+  weight: ["300", "400", "500", "600","700"],
   subsets: ["latin"],
 });
 
